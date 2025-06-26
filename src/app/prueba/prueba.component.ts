2af356ad71bd24be9d459ae0fc427cffc492c2fa
Herbert Daniel <herbert.bustamante@uab.edu.bo>
2025-06-26 10:07:12 -0400

    Instalado primeng, prime themes, tailwindcss

---
+++ src/app/prueba/prueba.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-prueba',
+  imports: [CardModule],
+  templateUrl: './prueba.component.html',
+  styleUrl: './prueba.component.scss',
+  standalone: true
+})
+export class PruebaComponent {
+
+}
